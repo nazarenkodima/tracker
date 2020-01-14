6cb2956400108891fc1c34e2c35d2249b3d063b1
Dmytro Nazarenko <dmamyk@gmail.com>
2020-01-14 18:56:53 +0200

    TRKR: + add logo

--- source/App/App.js
+++ source/App/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-import Layout from "../Components/Layout/Layout";
+import Layout from '../Components/Layout/Layout';
+import Logo from '../Components/Logo/Logo';
 
 @hot(module)
 export default class App extends Component {
   render() {
     return (
       <Layout>
-        <h1>tracker</h1>
+        <Logo />
       </Layout>
     );
   }
